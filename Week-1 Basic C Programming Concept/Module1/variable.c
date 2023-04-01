4cf8ffd95d6a7692b2b640e1c883ddd592f6d3af
skalaminhossain <skalaminhossain70@gmail.com>
2023-04-01 11:00:31 -0700

    added

---
+++ Week-1 Basic C Programming Concept/Module1/variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int rahim = 10;
+    float karim = 10.5;
+    char habib = 'Z';
+
+    printf("%d %f %c\n",rahim, karim,habib);
+
+    char myname[] = "Alamin";
+    int age = 22;
+    float salary = 20500.50;
+    printf("MY name is = %s\n" , myname);
+    printf("My age is = %d\n", age);
+    printf("Salary is = %0.2f" , salary);
+    return 0;
+}
