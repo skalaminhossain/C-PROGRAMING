de001cbada81f1b23b25f2b08de1fc44aeb4ff40
skalaminhossain <skalaminhossain70@gmail.com>
2023-04-01 09:48:07 -0700

    modified file

--- Week-1 Basic C Programming Concept/Module1/print.c
+++ Week-1 Basic C Programming Concept/Module1/print.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello World");
+    printf("Hello World\n");
+    printf("Bangladesh is a Beautifull Country\n");
+    printf("I am a programmer \n");
+    printf("I Am a ! \nhello world programmer");
     return 0;
 }
