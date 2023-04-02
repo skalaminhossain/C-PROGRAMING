d6aa64f9c165f21bf9c111732ffe77fda95ccc54
skalaminhossain <skalaminhossain70@gmail.com>
2023-04-02 10:37:09 -0700

    conditional

---
+++ Week-1 Basic C Programming Concept/Module2/conditional.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int tk ;
+    printf("Enter your tk :");
+    scanf("%d", &tk);
+    if(tk >= 100){
+        printf("Burgar khabo");
+    }else{
+        printf("Kissu Khabona");
+    }
+    return 0;
+}
---
+++ Week-1 Basic C Programming Concept/Module2/conditional2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int momey;
+    printf("Enter your money :");
+    scanf("%d",&momey);
+
+    if(momey >= 100){
+        printf("Burgar Khabo");
+    }else if(momey > 50){
+        printf("Fuska Khabo");
+    }else{
+        printf("TK na thakle ki ar khabo !");
+    }
+    return 0;
+}
