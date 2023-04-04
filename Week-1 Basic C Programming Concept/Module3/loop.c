c8deeb5389bb61fae65db4e567020596b70e0c28
skalaminhossain <skalaminhossain70@gmail.com>
2023-04-04 09:11:11 -0700

    for loop

---
+++ Week-1 Basic C Programming Concept/Module3/loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i = 0;
+    int count = 10;
+    for (i; i < count; i+=2)
+    {
+        printf("hello c %d \n",i );
+    }
+    
+    return 0;
+}
---
+++ Week-1 Basic C Programming Concept/Module3/loop1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    for (int i = 1; i <= 1000; i++)
+    {
+        printf("Number :%d\n",i);
+    }
+    
+    return 0;
+}
